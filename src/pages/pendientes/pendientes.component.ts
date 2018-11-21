5f8a2c2dc3628190767a7f31cd4a6358d9d545db
JoseRQ <mac@MacBook-Air-de-MAC.local>
2018-11-21 13:57:02 -0600

    add the first components

---
+++ src/pages/pendientes/pendientes.component.ts
@@ -0,0 +1,13 @@
+import {Component} from "@angular/core";
+import {DeseosService} from "../../providers/deseos.service";
+
+@Component({
+  selector: 'page-pendientes',
+  templateUrl: 'pendientes.component.html'
+})
+export class PendientesPage {
+
+  constructor(private deseosService: DeseosService) {
+
+  }
+}
